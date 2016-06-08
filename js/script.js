a88db9863b3db3735d08b58384fcfb4870874e83
Levi Hopkins and Noah Ramey <noah@>
2016-06-08 09:03:01 -0700

    add functionality and refactored javascript

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// BACK END //
+
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !==0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+
+// FRONT END //
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+
+    var year = parseInt($("#year").val());
+    var result = leapYear(year);
+
+    $("#true-false").text(result);
+  });
+});
